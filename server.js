f14cd75a44881309bf67d0c9b3ea40d46c6c8285
hemant-truemeds <hemant.jadhav@truemeds.in>
2023-04-26 11:12:02 +0530

    vercel setup

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const contactRouter = require("./routes/contactRoutes");
-const practiceRouter = require("./routes/practiceRoutes");
-const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middlerwares/errorHandle");
+// const contactRouter = require("./routes/contactRoutes");
+// const practiceRouter = require("./routes/practiceRoutes");
+// const connectDb = require("./config/dbConnection");
+// const errorHandler = require("./middlerwares/errorHandle");
 require("dotenv").config({});
 const fpJson = require("./constants/fp.json");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 // db connection
 // connectDb();
@@ -15,8 +15,8 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 
 // routes middlewares
-app.use("/api/contact", contactRouter);
-app.use("/api/practice", practiceRouter);
+// app.use("/api/contact", contactRouter);
+// app.use("/api/practice", practiceRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "hellow world" });
@@ -27,6 +27,6 @@ app.get("/poc_json/fp", (req, res) => {
   res.json(fpJson);
 });
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
